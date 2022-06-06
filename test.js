8754b165e9f59c0176eafc29a0388a883e655525
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-06-06 16:14:05 +0300

    fix test

--- test.js
+++ test.js
@@ -14,15 +14,34 @@ describe('List of tests', () => {
   });
 
   test('Test 1 ', async () => {
-    tracker.on
-      .any(({ method, sql, bindings }) => {
-        console.log(sql, bindings);
-        return true;
-      })
-      .response(() => {
-        return [];
-      });
+    tracker.on.select(({
+      sql,
+      bindings
+    }) => sql.includes('from "abc" where "abc"."id" in ($1)') && bindings.includes(1)).responseOnce([{
+      id: 1,
+      name: 'bla'
+    }]);
+
+    tracker.on.insert(({
+      sql,
+      bindings
+    }) => sql.includes('insert into "abc"') && bindings.includes(1)).responseOnce([{
+      id: 1,
+      name: 'bla'
+    }]);
+
+    tracker.on.update(({
+      sql,
+      bindings
+    }) => sql.includes('update "abc" set "x"') && bindings.includes(1)).responseOnce([{
+      id: 1,
+      name: 'bla'
+    }]);
 
     await functionOne();
+
+    expect(tracker.history.select).toHaveLength(1);
+    expect(tracker.history.insert).toHaveLength(1);
+    expect(tracker.history.update).toHaveLength(1);
   });
 });
